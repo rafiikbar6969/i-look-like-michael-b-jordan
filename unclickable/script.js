0121d194635b73a8393b27a7fc751482328802d8
uwurap <passeo.012345@gmail.com>
2024-02-24 20:47:50 +0800

    hehe

---
+++ unclickable/script.js
@@ -0,0 +1,8 @@
+const no = document.querySelector(".no");
+no.addEventListener("mouseover", function () {
+  const i = Math.ceil(Math.random() * 700 + 1);
+  const j = Math.ceil(Math.random() * 500 + 1);
+
+  no.style.left = i + "px";
+  no.style.top = j + "px";
+});
