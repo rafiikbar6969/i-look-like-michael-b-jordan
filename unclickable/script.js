7bddc6bcc0b6d31e918e638cd5dfba07aeec83be
uwurap <passeo.012345@gmail.com>
2024-03-19 23:50:22 +0800

    photo change and button

--- unclickable/script.js
+++ unclickable/script.js
@@ -1,8 +1,19 @@
 const no = document.querySelector(".no");
+no.addEventListener("click", function () {
+  const i = Math.ceil(Math.random() * 700 + 1);
+  const j = Math.ceil(Math.random() * 500 + 1);
+
+  no.style.left = i + "px";
+  no.style.top = j + "px";
+  no.innerHTML = "LMAO you cant click me click yes instead";
+});
+
 no.addEventListener("mouseover", function () {
   const i = Math.ceil(Math.random() * 700 + 1);
   const j = Math.ceil(Math.random() * 500 + 1);
 
   no.style.left = i + "px";
   no.style.top = j + "px";
+  no.innerHTML = "come click me";
 });
+console.log(no);
